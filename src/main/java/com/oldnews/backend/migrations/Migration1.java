dfac8ae8bd2039ddc3bd8d243299656c80ee2ed7
lpjahjah <lp.borgomoni@gmail.com>
2022-12-06 10:27:52 -0300

    build

--- src/main/java/com/oldnews/backend/migrations/Migration1.java
+++ src/main/java/com/oldnews/backend/migrations/Migration1.java
@@ -67,7 +67,7 @@ public class Migration1 implements Migration {
         return Flux
                 .fromIterable(articles)
                 .flatMap(imageFetchingUtil::fetchArticleImage)
-                .delayElements(Duration.ofSeconds(5))
+                .delayElements(Duration.ofSeconds(3))
                 .limitRate(10)
                 .doOnNext(finish())
                 .then(Mono.empty());
